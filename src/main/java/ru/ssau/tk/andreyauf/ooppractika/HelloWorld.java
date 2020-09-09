ec3e07d9a01c5e209603989a94bc6b3efefca4ee
Andrey-AUF <penskiy_a@bk,ru>
2020-09-09 14:20:43 +0400

    gg

--- src/main/java/ru/ssau/tk/andreyauf/ooppractika/HelloWorld.java
+++ src/main/java/ru/ssau/tk/andreyauf/ooppractika/HelloWorld.java
@@ -3,7 +3,7 @@ package ru.ssau.tk.andreyauf.ooppractika;
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello world");
+        System.out.println("Hello, world!");
 
     }
 }
