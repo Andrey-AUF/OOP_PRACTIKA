17ceab96ce0b8e836cc0e6100dbf72293e8841b3
andrejpenskij <penskiy_a@bk.ru>
2020-12-03 21:18:35 +0400

    scanner in

--- src/main/java/ru/ssau/tk/andreyauf/ooppractika/EX1.java
+++ src/main/java/ru/ssau/tk/andreyauf/ooppractika/EX1.java
@@ -1,4 +1,19 @@
 package ru.ssau.tk.andreyauf.ooppractika;
 
-public class EX1 {
+import java.util.Scanner;
+
+public class EX1{
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input name: ");
+        String name = in.nextLine();
+        System.out.print("Input age: ");
+        int age = in.nextInt();
+        System.out.print("Input height: ");
+        float height = in.nextFloat();
+        System.out.printf("Name: %s  Age: %d  Height: %.2f \n", name, age, height);
+        in.close();
+    }
 }
