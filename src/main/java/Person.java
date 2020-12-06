63a81a8d41a1dc2d19139ac7468d35e773333216
andrejpenskij <penskiy_a@bk.ru>
2020-12-06 22:10:46 +0400

    3.14 Переопределить в классе Person метод toString() так, чтобы он возвращал имя и фамилию, записанные через пробел. Покрыть метод тестами.

---
+++ src/main/java/Person.java
@@ -0,0 +1,17 @@
+import java.util.Date;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+
+    public Person(String firstName, String lastName, int passportId) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return firstName + (firstName.isEmpty() || lastName.isEmpty() ? "" : " ") + lastName;
+    }
+
+}
---
+++ src/test/java/PersonTest.java
@@ -0,0 +1,15 @@
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class PersonTest {
+
+    @Test
+    public void testTestToString() {
+        Person h = new Person("Andrey", "Penskiy", 862495137);
+        assertEquals(h.toString(), "Andrey Penskiy");
+
+
+    }
+}
