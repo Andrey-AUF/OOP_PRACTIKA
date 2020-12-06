aaf5d4b71e4ff252a171423d1222666aa2f155b9
andrejpenskij <penskiy_a@bk.ru>
2020-12-06 22:15:50 +0400

    3.15 Переопределить метод toString() в классе Point, чтобы он возвращал запись точки в формате [x, y, z]. Покрыть метод тестами.

---
+++ src/main/java/Point.java
@@ -0,0 +1,17 @@
+
+public class Point {
+    public final double x;
+    public final double y;
+    public final double z;
+
+    public Point(double x, double y, double z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    @Override
+    public String toString() {
+        return "[" + x + ", " + y + ", " + z + "]";
+    }
+}
---
+++ src/test/java/PointTest.java
@@ -0,0 +1,15 @@
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class PointTest {
+
+
+    @Test
+    public void testTestToString() {
+        assertEquals(new Point(1, 2, 3).toString(), "[1.0, 2.0, 3.0]");
+        assertEquals(new Point(1.1, 2.2, 3.3).toString(), "[1.1, 2.2, 3.3]");
+        assertEquals(new Point(-1.1, -2.2, -3.3).toString(), "[-1.1, -2.2, -3.3]");
+    }
+
+}
