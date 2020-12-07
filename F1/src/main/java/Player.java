475567c0d2b132ea4fd179152b6563b17cb1ea3c
andrejpenskij <penskiy_a@bk.ru>
2020-12-07 22:26:57 +0400

    "движение машины" но не долго

--- F1/src/main/java/Player.java
+++ F1/src/main/java/Player.java
@@ -3,12 +3,16 @@ import java.awt.*;
 
 public class Player {
     Image img = new ImageIcon("F1/res/2377 (1).png").getImage();
-    int v = 0;
+    int v = 4;
     int dv = 0;
     int s = 0;
     int layer1 = 0;
+    int x = 100;
+    int y = 30;
+
 
     public void move() {
         s += v;
+        layer1 -= v;
     }
 }
--- F1/src/main/java/Road.java
+++ F1/src/main/java/Road.java
@@ -1,12 +1,25 @@
-import javax.swing.ImageIcon;
-import javax.swing.JPanel;
+import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Road extends JPanel {
+public class Road extends JPanel implements ActionListener {
+    Timer mainTimer = new Timer(20, this);
     Image img = new ImageIcon("F1/res/doroga.jpg").getImage();
+    Player p = new Player();
+    public Road(){
+        mainTimer.start();
+    }
 
     public void paint(Graphics g) {
         g = (Graphics2D) g;
-g.drawImage(img,0,0,null);
+        g.drawImage(img, p.layer1, 0, null);
+        g.drawImage(p.img, p.x, p.y, null);
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        p.move();
+        repaint();
+
     }
 }
