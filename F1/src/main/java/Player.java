5f9603c5384d9e48d5d85694889dacbbdb56ec27
andrejpenskij <penskiy_a@bk.ru>
2020-12-08 13:42:51 +0400

    машина разгоняется

--- F1/src/main/java/Player.java
+++ F1/src/main/java/Player.java
@@ -4,7 +4,7 @@ import java.awt.event.KeyEvent;
 
 public class Player {
     Image img = new ImageIcon("F1/res/2377 (1).png").getImage();
-    int v = 40;
+    int v = 0;
     int dv = 0;
     int s = 0;
     int layer1 = 0;
@@ -15,6 +15,7 @@ public class Player {
 
     public void move() {
         s += v;
+        v += dv;
         if (layer2 - v < 0) {
             layer1 = 0;
             layer2 = 4608;
@@ -24,12 +25,25 @@ public class Player {
             layer2 -= v;
         }
     }
-    public void keyPressed(KeyEvent e){
-        JOptionPane.showMessageDialog(null,"key pressed");
+
+    public void keyPressed(KeyEvent e) {
+        int key = e.getKeyCode();
+        if (key == KeyEvent.VK_RIGHT) {
+            dv = 1;
+        }
+        if (key == KeyEvent.VK_LEFT) {
+            dv = -1;
+        }
+        if (key == KeyEvent.VK_UP) {
+            dv = 1;
+        }
 
     }
-    public void keyReleased(KeyEvent e){
-        JOptionPane.showMessageDialog(null,"key realised");
 
+    public void keyReleased(KeyEvent e) {
+        int key = e.getKeyCode();
+        if (key == KeyEvent.VK_RIGHT || key == KeyEvent.VK_LEFT) {
+            dv = 0;
+        }
     }
 }
