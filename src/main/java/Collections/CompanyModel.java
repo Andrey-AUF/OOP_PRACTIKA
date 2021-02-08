eb894033559f6b76c256c1b3c0041446adac8353
andrejpenskij <penskiy_a@bk.ru>
2021-02-08 11:16:18 +0400

    5.10

--- src/main/java/Collections/CompanyModel.java
+++ src/main/java/Collections/CompanyModel.java
@@ -1,12 +1,15 @@
 package Collections;
 
 import java.util.Collection;
+import java.util.Date;
 import java.util.LinkedHashSet;
+import java.util.List;
 
 public class CompanyModel {
     private final Collection<Location> allLocations;
     private final Collection<Route> allRoutes;
     private final Collection<Driver> allDrivers;
+    private int maxId = 0;
 
     public CompanyModel() {
         allLocations = new LinkedHashSet<>();
@@ -24,5 +27,58 @@ public class CompanyModel {
 
     public Collection<Driver> getAllDrivers() {
         return allDrivers;
+    }
+    public Settlement addSettlement(String name, double latitude, double longitude, SettlementType type, int population) {
+        Settlement settlement = new Settlement();
+
+        settlement.setId(++maxId);
+        settlement.setName(name);
+        settlement.setLatitude(latitude);
+        settlement.setLongitude(longitude);
+        settlement.setType(type);
+        settlement.setSettlement(population);
+
+        allLocations.add(settlement);
+
+        return settlement;
+    }
+
+    public Waypoint addWaypoint(String name, double latitude, double longitude, WaypointType type, Settlement settlement) {
+        Waypoint waypoint = new Waypoint();
+
+        waypoint.setId(++maxId);
+        waypoint.setName(name);
+        waypoint.setLatitude(latitude);
+        waypoint.setLongitude(longitude);
+        waypoint.setType(type);
+        waypoint.setSettlement(settlement);
+
+        allLocations.add(waypoint);
+
+        return waypoint;
+    }
+
+    public Driver addDriver(String name, Date licenseExpirationDate) {
+        Driver driver = new Driver();
+
+        driver.setId(++maxId);
+        driver.setName(name);
+        driver.setLicenseExpirationDate(licenseExpirationDate);
+
+        allDrivers.add(driver);
+
+        return driver;
+    }
+
+    public Route addRoute(List<Location> locations) {
+        Route route = new Route();
+
+        for (Location location : locations) {
+            route.addLocation(location);
+        }
+
+        allRoutes.add(route);
+
+        return route;
     }
 }
